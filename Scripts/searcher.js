8e356e23944ecccebdb54e49ed7a242449ca7738
karlinskid <akarlisky@msn.com>
2015-08-01 15:36:14 -0400

    initial commit

---
+++ Scripts/searcher.js
@@ -0,0 +1,30 @@
+(function() {
+	angular.module("searcher", ['ngAnimate'])
+		   .controller( "FormController", function($scope, $http){
+				var getUserDetails = function(response){
+					$scope.user = response.data;
+					$http.get( $scope.user.repos_url )
+						 .then( getRepository );
+				};
+					
+				var getRepository = function(response){
+					$scope.repos = response.data;
+				};
+					
+				$scope.search = function(username){
+					$http.get("https://api.github.com/users/" + username)
+						 .then(getUserDetails);
+				};
+			
+				$scope.direction = true;
+				$scope.sortOrder = "name";
+				$scope.sort = function(column) {
+					if ($scope.sortOrder === column) {
+						$scope.direction = !$scope.direction;
+					} else {
+						$scope.sortOrder = column;
+						$scope.direction = true;
+					}
+				};
+			});
+}());
